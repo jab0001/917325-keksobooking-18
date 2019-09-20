4f2fe327d47077cf846d00d9c08c6c6771df7f74
Pavel <Jab001@ya.ru>
2019-09-20 21:09:26 +0300

    добавляет исправленияпо фотографиям и координатам

--- js/main.js
+++ js/main.js
@@ -2,50 +2,53 @@
 
 var OFFER_TITLE = 'Напишите функцию для создания массива из 8 сгенерированных JS объектов.';
 var OFFER_AMOUNTS = 8;
-var OFFER_ADDRESS = '600, 350';
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var OFFER_ROOMS = [1, 2, 3, 100];
 var OFFER_GUESTS = [1, 2, 3, 0];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var OFFER_TIME = [12, 13, 14];
+var OFFER_TIME = ['12:00', '13:00', '14:00'];
 var pinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 var mapItem = document.querySelector('.map');
-
-mapItem.classList.remove('map--faded');
 
 var getRandomArrayElement = function (array) {
   var random = array[Math.floor(Math.random() * array.length)];
   return random;
 };
 
-var getRandomElements = function (min, max) {
+var getRandomElement = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getObjMock = function (author, title, address, type, rooms, guests, features, photos, time) {
+var PHOTOS = [];
+for (var k = 0; k < 3; k++) {
+  PHOTOS.push(getRandomArrayElement(OFFER_PHOTOS));
+}
+
+var getObjMock = function (author, title, type, rooms, guests, features, photos, time) {
+  var offerAdress = [getRandomElement(20, 80), getRandomElement(130, 630)];
   var object = ({
     author: {
       avatar: 'img/avatars/user0' + (author + 1) + '.png'
     },
     offer: {
       title: title,
-      address: getRandomArrayElement(address),
-      price: getRandomElements(1000, 10000),
+      address: offerAdress,
+      price: getRandomElement(1000, 10000),
       type: getRandomArrayElement(type),
       rooms: getRandomArrayElement(rooms),
       guests: getRandomArrayElement(guests),
-      checkin: getRandomArrayElement(time) + ':00',
-      checkout: getRandomArrayElement(time) + ':00',
+      checkin: getRandomArrayElement(time),
+      checkout: getRandomArrayElement(time),
       features: getRandomArrayElement(features),
       description: title,
-      photos: getRandomArrayElement(photos)
+      photos: PHOTOS
     },
     location: {
-      x: getRandomElements(0, 100),
-      y: getRandomElements(0, 100)
+      x: offerAdress[0],
+      y: offerAdress[1]
     }
   });
   return object;
@@ -53,18 +56,20 @@ var getObjMock = function (author, title, address, type, rooms, guests, features
 
 var objects = [];
 for (var i = 0; i < OFFER_AMOUNTS; i++) {
-  objects.push(getObjMock(i, OFFER_TITLE, OFFER_ADDRESS, OFFER_TYPE, OFFER_ROOMS, OFFER_GUESTS, OFFER_FEATURES, OFFER_PHOTOS, OFFER_TIME));
+  objects.push(getObjMock(i, OFFER_TITLE, OFFER_TYPE, OFFER_ROOMS, OFFER_GUESTS, OFFER_FEATURES, PHOTOS, OFFER_TIME));
 }
 
-var renderPinFromTemplate = function (coordinate) {
+console.log(objects);
+
+var renderPinFromTemplate = function (offers) {
   var pin = pinTemplate.cloneNode(true);
 
-  pin.style.left = coordinate.location.x + '%';
-  pin.style.top = coordinate.location.y + '%';
+  pin.style.left = offers.location.x + '%';
+  pin.style.top = offers.location.y + 'px';
 
   var pinImg = pin.querySelector('img');
-  pinImg.src = coordinate.author.avatar;
-  pinImg.alt = coordinate.offer.title;
+  pinImg.src = offers.author.avatar;
+  pinImg.alt = offers.offer.title;
 
   return pin;
 };
@@ -79,3 +84,8 @@ var renderPins = function (arr) {
 
 var pinContainerElem = mapItem.querySelector('.map__pins');
 pinContainerElem.appendChild(renderPins(objects));
+
+mapItem.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapItem.classList.remove('map--faded');
+});
